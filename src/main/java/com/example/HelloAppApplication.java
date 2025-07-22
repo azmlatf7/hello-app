f63d029e5b2a227d21061be898f7897b139d6ed8
azmlatf7 <muhdazim5413@gmail.com>
2025-07-22 10:41:00 +0800

    Update HelloAppApplication.java

--- src/main/java/com/example/HelloAppApplication.java
+++ src/main/java/com/example/HelloAppApplication.java
@@ -16,7 +16,7 @@ public class HelloAppApplication {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, DevOps! Donezo";
+        return "Hello, DevOps! Donezo Bonezo";
     }
 }
 
